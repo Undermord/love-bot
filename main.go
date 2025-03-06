64f342a98d673479032d69cfe732ca7a5981613f
Vladislav A. Gusev <v.gusev@hserm.ru>
2025-03-07 03:37:48 +0400

    Initial commit

--- main.go
+++ main.go
@@ -5,7 +5,22 @@ import (
 	"os"
 )
 
+func initLogging() {
+	// Открываем файл для записи логов
+	logFile, err := os.OpenFile("lovebot.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Ошибка при открытии файла логов: %v", err)
+	}
+
+	log.SetOutput(logFile)
+
+	log.SetFlags(log.Ldate | log.Ldate | log.Lshortfile)
+
+	log.Println("Логирование инициализировано")
+}
+
 func main() {
+	initLogging()
 	log.Println("Инициализация конфигурации бота")
 
 	// Получаем токен из переменных окружения или используем значение по умолчанию
